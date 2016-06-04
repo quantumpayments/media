530e2bc59819d3d018ee6447d48d16d4e0b89383
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-04 14:57:27 +0200

    Add media

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = require('./lib/qpm_media')
 module.exports.createServer = require('./lib/create-server')
-module.exports.handlers = { random : require('./lib/handlers/random') }
+module.exports.handlers = {
+  addMedia : require('./lib/handlers/addmedia'), 
+  random : require('./lib/handlers/random')
+}
