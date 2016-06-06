2a208d6fcc396c37ae5f094af5cc7095e7567aa9
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-06 19:00:36 +0200

    Rating table

---
+++ model/Rating.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Rating` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uri` varchar(767) DEFAULT NULL,
+  `rating` double DEFAULT NULL,
+  `votes` double DEFAULT NULL,
+  `reviewer` VARCHAR(767),
+  `source` VARCHAR(767),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `constr_uri` (`uri`)
+)
