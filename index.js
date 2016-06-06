e16893716e030ea039e517f5c33436833b9b589f
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-06 22:48:25 +0200

    add to index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = require('./lib/qpm_media')
 module.exports.handlers = {
   addMedia : require('./lib/handlers/addmedia'),
-  random : require('./lib/handlers/random')
+  random : require('./lib/handlers/random'),
+  rate : require('./lib/handlers/rate')
 }
