db7956a20793e56e8a38b706bf3f584df45c844c
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-08 16:14:55 +0200

    update last seen

--- bin/view.js
+++ bin/view.js
@@ -1,41 +1,72 @@
 #!/usr/bin/env node
 
 // requires
-var child_process = require('child_process');
+var child_process = require('child_process')
 var fs            = require('fs')
 var program       = require('commander')
 var qpm_media     = require('../')
-var solidbot      = require('solidbot');
-var wc_db         = require('wc_db')
+var request       = require('request')
 
 /**
- * version as a command
- */
+* version as a command
+*/
 function bin(argv) {
   // setup config
-  var config = require('../config/config.js')
 
-  program
-  .option('-d, --database <database>', 'Database')
-  .parse(argv)
 
-  var defaultDatabase = 'media'
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-  config.database = program.database || config.database || defaultDatabase
-  var url = process.argv[2] || 'https://www.reddit.com/r/perfectloops/top/?sort=top&t=all&count=25&after=t3_4j0v4f'
+  var url     = process.argv[2] || 'https://localhost:3000/random_rate'
+  var cert    = process.argv[3] || process.env['CERT']
+  var display = process.argv[4] || process.env['DISP'] || 'display'
 
-  qpm_media.getRandomImage(function(err, ret){
-    if (err) {
-      console.error(err)
-    } else {
-      console.log(ret[0][0].uri)
-      // TODO : switch on OS and tools
-      var cmd = 'feh -x -F  -D 3 -Z --cycle-once ' + ret[0][0].uri
-      child_process.exec(cmd)
+  var options = {
+    url: url,
+    key: fs.readFileSync(cert),
+    cert: fs.readFileSync(cert),
+    headers: { //We can define headers too
+      'Accept': 'application/json'
     }
+  }
+
+  var config = require('../config/config.js')
+  console.log(config);
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+      json = JSON.parse(body)
+      var uri = json.uri
+      var cacheURI = json.cacheURI
+      var displayURI = cacheURI || uri
+      console.log(json.rating)
+      console.log(displayURI)
+
+      var cmd = display + ' ' + displayURI
+      child_process.exec(cmd)
+      // main
+      var params = {}
+      params.uri = uri
+      qpm_media.updateLastSeen(params, config).then(function(ret) {
+        if (ret.conn) {
+          var conn = ret.conn
+          conn.close()
+        }
+        console.log(ret.ret)
+      }).catch(function(err){
+        if (err.conn) {
+          var conn = err.conn
+          conn.close()
+        }
+        console.error(err.err)
+      })
+
+
+    } else {
+      console.error(error);
+    }
+
   })
-
-
 }
 
 // If one import this file, this is a module, otherwise a library
