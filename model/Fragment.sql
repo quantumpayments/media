38cda796e7f16b60c679b113dc749207218fd829
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-09 08:02:26 +0200

    Fragment

---
+++ model/Fragment.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `CREATE TABLE `Fragment` (
+  `id` int(11) DEFAULT NULL,
+  `start` double DEFAULT NULL,
+  `end` double DEFAULT NULL,
+  `rating` double DEFAULT NULL,
+  `lastSeen` datetime DEFAULT NULL,
+  KEY `fragment_id` (`id`)
+)
