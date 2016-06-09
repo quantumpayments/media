a10b1dc5d1f839e5a12bd1f8b7d1a5fae3ecbc3b
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-09 15:05:08 +0200

    search

---
+++ bin/search.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// requires
+require('console.table')
+var fs         = require('fs')
+var program    = require('commander')
+var wc_db      = require('wc_db')
+var qpm_media  = require('../')
+var solidbot   = require('solidbot');
+
+/**
+ * version as a command
+ */
+function bin(argv) {
+  // setup config
+  var config = require('../config/config.js')
+
+  program
+  .option('-d, --database <database>', 'Database')
+  .parse(argv)
+
+  var defaultDatabase = 'media'
+
+  config.database = program.database || config.database || defaultDatabase
+  var q = process.argv[2] || ''
+
+  params = {}
+  params.q = q
+
+  qpm_media.search(params).then(function(ret){
+    console.table(ret.ret[0])
+    ret.conn.close()
+  }).catch(function (err) {
+    console.error(err)
+  })
+
+
+}
+
+// If one import this file, this is a module, otherwise a library
+if (require.main === module) {
+  bin(process.argv)
+}
+
+module.exports = bin
