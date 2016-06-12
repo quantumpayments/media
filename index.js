70c8fe156c9a8eeff36cdd1d5eb0449a739aed3e
Melvin Carvalho <melvincarvalho@gmail.com>
2016-06-12 14:49:41 +0200

    add audio to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports.handlers = {
   addMedia : require('./lib/handlers/addmedia'),
   random : require('./lib/handlers/random'),
   random_rate : require('./lib/handlers/random_rate'),
+  random_rate_audio : require('./lib/handlers/random_rate_audio'),
   rate : require('./lib/handlers/rate'),
   top : require('./lib/handlers/search'),
   tag : require('./lib/handlers/tag'),
