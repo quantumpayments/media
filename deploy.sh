2ab040ad93afe221592818e1ae0c39c988345792
Melvin Carvalho <melvincarvalho@gmail.com>
2016-08-03 16:15:22 +0200

    fix costs and deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ git add -u
 git commit -m "$1"
 git push origin master
 
-git checkout gh-pages
-git merge master
-git push origin gh-pages
-
-git checkout master
+npm --no-git-tag-version version patch
+npm publish
 
