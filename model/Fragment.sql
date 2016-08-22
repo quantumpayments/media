36ed5aa77cba5231a42a156dc16ba3a32fc1c886
Melvin Carvalho <melvincarvalho@gmail.com>
2016-08-22 18:25:00 +0200

    Add default

--- model/Fragment.sql
+++ model/Fragment.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Fragment` (
   `end` double DEFAULT NULL,
   `rating` double DEFAULT NULL,
   `lastSeen` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT '1' NULL,
   KEY `fragment_id` (`id`),
   UNIQUE KEY `unique_uri` (`id`, `start`, `end`, `user_id`)
 )
