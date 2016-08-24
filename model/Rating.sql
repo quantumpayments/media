499f2ff9edd80cec1063c0ddbebe621b0e542946
Melvin Carvalho <melvincarvalho@gmail.com>
2016-08-24 15:21:14 +0200

    Use rating table for insert

--- model/Rating.sql
+++ model/Rating.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Rating` (
   `uri` varchar(767) DEFAULT NULL,
   `rating` double DEFAULT NULL,
   `votes` double DEFAULT NULL,
-  `reviewer_id` INTEGER(11),
+  `reviewer_id` INTEGER(11) DEFAULT '1' NULL,
   `source` VARCHAR(767),
   `datePublished` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
