6370ef105d0b04b17be6e3487959239b1378de80
Melvin Carvalho <melvincarvalho@gmail.com>
2016-08-25 22:44:19 +0200

    getlength script

---
+++ bin/getlength.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ls "$1"
+
+L=$(  ffmpeg -i "$1" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }' )
+
+echo $L
