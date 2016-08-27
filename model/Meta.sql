52579823be776d11f8fd5e7f5cac27c4f351b221
Melvin Carvalho <melvincarvalho@gmail.com>
2016-08-27 14:45:14 +0200

    Add subtitlesURI and charenc to model

--- model/Meta.sql
+++ model/Meta.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `Meta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `length` INTEGER(11) DEFAULT NULL,
+  `subtitlesURI` varchar(700) DEFAULT NULL,
+  `charenc` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
